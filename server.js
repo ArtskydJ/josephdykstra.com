d3062725c837e7fe725508e003c7e36e250c490f
Joseph Dykstra <josephdykstra@gmail.com>
2014-10-24 08:12:52 -0500

    made more readable and better error messages

--- server.js
+++ server.js
@@ -3,19 +3,14 @@ var send = require('send')
 var config = require('./serverConfig.json')
 var PORT = process.env.PORT || config.port || 80
 
-var server = http.createServer().listen(PORT)
-
-server.on('request', function (req, res) {
-	config.verbose && console.log('accessing:', req.url)
-	send(req, req.url, config.send).pipe(res)
-})
-
-server.on('error', function (err) {
-	if (err.code == 'EADDRINUSE') {
-		console.log('Address in use, retrying...');
-		setTimeout(function () {
-			try { server.close() } catch (e) { console.error(e) }
-			server.listen(PORT);
-		}, 1000);
-	}
-})
+http
+	.createServer()
+	.listen(PORT)
+	.on('request', function (req, res) {
+		config.verbose && console.log('accessing:', req.url)
+		send(req, req.url, config.send).pipe(res)
+	})
+	.on('error', function (err) {
+		if (err.code == 'EADDRINUSE') console.log('Close the server running on port '+PORT+' and try again.')
+		else console.dir(err)
+	})
