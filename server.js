7ff852a00103920bc989ca3e0607dee500c62b93
Joseph Dykstra <josephdykstra@gmail.com>
2014-10-24 08:05:20 -0500

    fix server.js

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var send = require('send')
 var config = require('./serverConfig.json')
 var PORT = process.env.PORT || config.port || 80
 
-http.createServer(function (req, res) {
+var server = http.createServer().listen(PORT)
+
+server.on('request', function (req, res) {
 	config.verbose && console.log('accessing:', req.url)
 	send(req, req.url, config.send).pipe(res)
-}).listen(PORT).on('error', function (err) {
+})
+
+server.on('error', function (err) {
 	if (err.code == 'EADDRINUSE') {
 		console.log('Address in use, retrying...');
 		setTimeout(function () {
