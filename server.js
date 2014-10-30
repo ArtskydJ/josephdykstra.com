ad5d4e76cb555f30cb2b479dd5231db36ee51276
Joseph Dykstra <josephdykstra@gmail.com>
2014-10-29 22:15:42 -0500

    use node-static instead of send

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var http = require('http')
-var send = require('send')
+var Static = require('node-static')
 var config = require('./serverConfig.json')
 var PORT = process.env.PORT || config.port || 80
 
-http
-	.createServer()
-	.listen(PORT)
-	.on('request', function (req, res) {
-		config.verbose && console.log('accessing:', req.url)
-		send(req, req.url, config.send).pipe(res)
+var fileServer = new Static.Server(config.staticDir, {gzip: true})
+
+http.createServer(function (req, res) {
+	config.verbose && console.log('accessing:', req.url)
+	fileServer.serveFile(req.url, 200, {}, req, res).on('error', function (err) {
+		res.writeHead(err.status, err.headers)
+		res.end(err.message)
 	})
-	.on('error', function (err) {
-		if (err.code == 'EADDRINUSE') console.log('Close the server running on port '+PORT+' and try again.')
-		else console.dir(err)
-	})
+}).listen(PORT).on('error', function (err) {
+	if (err.code == 'EADDRINUSE') console.log('Close the server running on port '+PORT+' and try again.')
+	else console.dir(err)
+})
