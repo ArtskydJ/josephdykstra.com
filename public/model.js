2a48f299d01b73b5f3fb85f37a172949a19ee1ce
Joseph Dykstra <josephdykstra@gmail.com>
2014-11-18 09:37:55 -0600

    Structure dirs

---
+++ public/model.js
@@ -0,0 +1,79 @@
+var Ractive = require('ractive')
+var Renderer = require('noddity-renderer')
+
+function cbIfErr(onErr, noErr) {
+	return function (err) {
+		if (err && !err.notFound) onErr(err)
+		else noErr.apply(null, [].slice.call(arguments, 1))
+	}
+}
+
+function formatButlerPostList(posts) {
+	return posts.reverse().filter(function(post) {
+		return typeof post.metadata.title === 'string'
+	}).map(function(post) {
+		return {
+			title: post.metadata.title,
+			filename: post.filename.replace(/\.md$/, '')
+		}
+	})
+}
+
+module.exports = function Model(ractiveTemplate, ractiveData, butler, renderer) {
+	var ractive = new Ractive({
+		el: '',
+		template: ractiveTemplate,
+		data: Object.create(ractiveData)
+	})
+
+	function getPostListFromButler(cb) {
+		cb = cb || function () {}
+		butler.getPosts(cbIfErr(cb, function (posts) {
+			var postList = formatButlerPostList(posts)
+			ractive.set('postList', postList)
+			cb(null, postList)
+		}))
+	}
+
+	//If not has post list: get it from butler
+	//return the post list
+	function getCurrentPostList(cb) {
+		cb = cb || function () {}
+		var postList = ractive.get('postList')
+		if (postList) {
+			process.nextTick(cb.bind(null, null, postList))
+		} else {
+			getPostListFromButler(cb)
+		}
+	}
+
+	function setCurrent(key, cb) {
+		butler.getPost(key, cbIfErr(cb, function (post) {
+			ractive.set('page', post.metadata.title)
+			ractive.set('date', post.metadata.date)
+			getCurrentPostList(function () {
+				renderer.renderPost(post, cbIfErr(cb, function (html) {
+					ractive.set('html', html)
+					cb(null, ractive.toHTML())
+				}))
+			})
+		}))
+	}
+
+	function onPostChanged(key, newValue, oldValue) { //oldValue does nothing right now
+		function titleHasChanged(postListItem) {
+			return postListItem.filename === key && postListItem.title !== newValue.metadata.title
+		}
+
+		var postList = ractive.get('postList')
+		if (postList && postList.some(titleHasChanged)) {
+			getPostListFromButler()
+		}
+	}
+
+	butler.on('post changed', onPostChanged)
+	butler.on('index changed', getCurrentPostList)
+	return {
+		setCurrent: setCurrent
+	}
+}
