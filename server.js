4d4f00aa40e09639b66fd9bf03b9a680de7fd1c4
Joseph Dykstra <josephdykstra@gmail.com>
2015-04-28 22:11:11 -0500

    split out view-model.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 var http = require('http')
 var St = require('st')
 var url = require('url')
-var noddityConfig = require('./noddity-config.json')
 
 //Constants
 var DIR = './web/'
@@ -18,21 +17,7 @@ var defaultStatic = St({
 })
 
 //Noddity
-var viewModel = (function () {
-	var Level = require('level-mem')
-	var Retrieval = require('noddity-fs-retrieval')
-	var Butler = require('noddity-butler')
-	var Renderer = require('noddity-renderer')
-	var ViewModel = require('noddity-view-model')
-	var renderData = require('./render-data.json')
-	var renderTemplate = require('fs').readFileSync(DIR + 'index.html', {encoding:'utf8'})
-
-	var db = new Level('./database')
-	var retrieve = new Retrieval(noddityConfig.root)
-	var butler = new Butler(retrieve, db, noddityConfig.butler)
-	var renderer = new Renderer(butler, String)
-	return new ViewModel(butler, renderer, renderTemplate, renderData)
-})()
+var viewModel = require('./view-model.js')()
 
 function renderPage(path, res, onFail) {
 	viewModel(path, function (err, html) {
---
+++ view-model.js
@@ -0,0 +1,17 @@
+﻿var noddityConfig = require('./noddity-config.json')
+
+module.exports = function VModel() {
+	var Level = require('level-mem')
+	var Retrieval = require('noddity-fs-retrieval')
+	var Butler = require('noddity-butler')
+	var Renderer = require('noddity-renderer')
+	var ViewModel = require('noddity-view-model')
+	var renderData = require('./render-data.json')
+	var renderTemplate = require('fs').readFileSync(DIR + 'index.html', {encoding:'utf8'})
+
+	var db = new Level('./database')
+	var retrieve = new Retrieval(noddityConfig.root)
+	var butler = new Butler(retrieve, db, noddityConfig.butler)
+	var renderer = new Renderer(butler, String)
+	return new ViewModel(butler, renderer, renderTemplate, renderData)
+}
