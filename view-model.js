97bbb39308f2b8237af8dc146c35873807c4b39d
Joseph Dykstra <josephdykstra@gmail.com>
2015-06-05 21:08:58 -0500

    moved require()s out of closure

--- view-model.js
+++ view-model.js
@@ -1,15 +1,14 @@
 ﻿var noddityConfig = require('./noddity-config.json')
 var fs = require('fs')
+var Level = require('level-mem')
+var Retrieval = require('noddity-retrieval')
+var Butler = require('noddity-butler')
+var Renderer = require('noddity-renderer')
+var ViewModel = require('noddity-view-model')
+var renderData = require('./render-data.json')
 
 module.exports = function VModel(templateDir) {
-	var Level = require('level-mem')
-	var Retrieval = require('noddity-retrieval')
-	var Butler = require('noddity-butler')
-	var Renderer = require('noddity-renderer')
-	var ViewModel = require('noddity-view-model')
-	var renderData = require('./render-data.json')
 	var renderTemplate = fs.readFileSync(templateDir, { encoding:'utf8' })
-
 	var db = new Level('./database')
 	var retrieve = new Retrieval(noddityConfig.root)
 	var butler = new Butler(retrieve, db, noddityConfig.butler)
