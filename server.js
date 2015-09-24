c0e67154e18bfb9617f8e08efd73e79ddee39c93
Joseph Dykstra <josephdykstra@gmail.com>
2015-09-24 08:24:47 -0500

    Use new renderer

--- server.js
+++ server.js
@@ -2,55 +2,46 @@
 var http = require('http')
 var Ecstatic = require('ecstatic')
 var url = require('url')
+var viewModel = require('./view-model.js')()
 
-//Constants
-var DIR = './web/'
+var DIR = __dirname + '/web/'
 var PORT = process.argv[2] || 80
 
-//Static File Server
-var defaultStatic = Ecstatic({
+var serveAssets = Ecstatic({
 	root: DIR,
+	autoIndex: false,
 	handleError: false,
 	gzip: true // needs .gz files
 })
 
-//Noddity
-var viewModel = require('./view-model.js')(DIR + 'index.html')
-
-function renderPage(path, res, onFail) {
-	viewModel(path, function (err, html) {
-		if (!err) {
-			res.writeHead(200)
-			res.end(html, 'utf8')
-		} else {
-			onFail && onFail()
-		}
-	})
-}
-
-function errorResponse(errorString) {
-	if (!errorString) errorString = 'An unknown error occurred.'
-	return function failure(err) {
-		res.writeHead(500)
-		res.end(err ? err.message : errorString, 'utf8')
-	}
-}
-
-//Routing
-function route(req, res) {
-	var path = url.parse(req.url).pathname.slice(1) || 'index'
-	renderPage(path, res, function fail1() {
-		defaultStatic(req, res, function fail2 () {
-			renderPage('404', res, errorResponse('failed to generate page'))
-		})
-	})
-}
-
-//Server
-var server = http.createServer(route)
+var server = http.createServer()
 server.listen(PORT)
 server.on('error', function (err) {
 	(err.code == 'EADDRINUSE') ?
 		console.log('A server is already running on '+PORT+'.') :
 		console.error('HTTP Server error:', err)
+})
+
+function renderPage(filename, res, statusCode, onFail) {
+	viewModel(filename, function (err, html) {
+		if (!err) {
+			res.writeHead(statusCode)
+			res.end(html, 'utf8')
+		} else if (typeof onFail === 'function') {
+			onFail()
+		} else {
+			res.writeHead(500)
+			res.end(err ? err.message : 'An unknown error occurred', 'utf8')
+		}
+	})
+}
+
+server.on('request', function route(req, res) {
+	var path = url.parse(req.url).pathname.slice(1) || 'index'
+
+	renderPage(path, res, 200, function fail1() {
+		serveAssets(req, res, function fail2 () {
+			renderPage('404', res, 404)
+		})
+	})
 })
--- view-model.js
+++ view-model.js
@@ -1,17 +1,34 @@
-﻿var noddityConfig = require('./noddity-config.json')
 var fs = require('fs')
 var Level = require('level-mem')
-var Retrieval = require('noddity-retrieval')
 var Butler = require('noddity-butler')
-var Renderer = require('noddity-renderer')
-var ViewModel = require('noddity-view-model')
+var Linkifier = require('noddity-linkifier')
+var render = require('noddity-render-static')
 var renderData = require('./render-data.json')
+var extend = require('xtend')
 
-module.exports = function VModel(templateDir) {
-	var renderTemplate = fs.readFileSync(templateDir, { encoding:'utf8' })
-	var db = new Level('./database')
-	var retrieve = new Retrieval(noddityConfig.root)
-	var butler = new Butler(retrieve, db, noddityConfig.butler)
-	var renderer = new Renderer(butler, String)
-	return new ViewModel(butler, renderer, renderTemplate, renderData)
+module.exports = function VModel() {
+	var noddityRoot = 'https://raw.githubusercontent.com/ArtskydJ/josephdykstra.com-content/master/'
+	var butler = new Butler(noddityRoot, new Level(), {
+		refreshEvery: 600000,
+		cacheCheckIntervalMs: 10000
+	})
+	var postTemplate = null
+	butler.getPost('post-template.html', function (err, post) {
+		if (err) throw err
+		postTemplate = post
+	})
+	return function (filename, cb) {
+		var renderOpts = {
+			butler: butler,
+			linkifier: Linkifier('/'),
+			data: renderData // not both places
+		}
+		if (filename.indexOf('.') == -1) filename += '.md'
+
+		var post = extend(postTemplate)
+		post.metadata = extend(renderData, post.metadata)
+		post.content = post.content.replace('{{{html}}}', '::' + filename + '::')
+
+		render(post, renderOpts, cb)
+	}
 }
