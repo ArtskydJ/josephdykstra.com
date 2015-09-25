ac52416ebb76326c073871e55f84e07ecf67e8ff
Joseph Dykstra <josephdykstra@gmail.com>
2015-09-24 23:47:41 -0500

    Finalized use-new-renderer branch

--- server.js
+++ server.js
@@ -6,42 +6,51 @@ var viewModel = require('./view-model.js')()
 
 var DIR = __dirname + '/web/'
 var PORT = process.argv[2] || 80
+var MARKDOWN_FILE_RE = /^(?:(.+)\.md|([^\.]+))$/
 
 var serveAssets = Ecstatic({
 	root: DIR,
-	autoIndex: false,
-	handleError: false,
-	gzip: true // needs .gz files
+	autoIndex: false
 })
 
 var server = http.createServer()
 server.listen(PORT)
 server.on('error', function (err) {
-	(err.code == 'EADDRINUSE') ?
-		console.log('A server is already running on '+PORT+'.') :
+	if (err.code === 'EADDRINUSE') {
+		console.log('A server is already running on ' + PORT)
+	} else {
 		console.error('HTTP Server error:', err)
+	}
 })
 
-function renderPage(filename, res, statusCode, onFail) {
+function renderPage(filename, res, statusCode) {
 	viewModel(filename, function (err, html) {
-		if (!err) {
-			res.writeHead(statusCode)
-			res.end(html, 'utf8')
-		} else if (typeof onFail === 'function') {
-			onFail()
+		if (err) {
+			if (statusCode !== 404) { // disallow 404 recursion
+				renderPage('404.md', res, 404)
+			} else {
+				res.writeHead(500)
+				res.end(err ? err.message : 'An unknown error occurred', 'utf8')
+			}
 		} else {
-			res.writeHead(500)
-			res.end(err ? err.message : 'An unknown error occurred', 'utf8')
+			res.writeHead(statusCode || 200)
+			res.end(html, 'utf8')
 		}
 	})
 }
 
 server.on('request', function route(req, res) {
-	var path = url.parse(req.url).pathname.slice(1) || 'index'
-
-	renderPage(path, res, 200, function fail1() {
-		serveAssets(req, res, function fail2 () {
-			renderPage('404', res, 404)
-		})
-	})
+	var filename = urlToFilename(req.url)
+	if (MARKDOWN_FILE_RE.test(filename)) {
+		renderPage(filename, res)
+	} else {
+		serveAssets(req, res)
+	}
 })
+
+function urlToFilename(urlStr) {
+	return url.parse(urlStr).pathname
+		.slice(1)
+		.replace(MARKDOWN_FILE_RE, '$1$2.md')
+		|| 'index.md'
+}
--- view-model.js
+++ view-model.js
@@ -1,4 +1,3 @@
-var fs = require('fs')
 var Level = require('level-mem')
 var Butler = require('noddity-butler')
 var Linkifier = require('noddity-linkifier')
@@ -7,28 +6,27 @@ var renderData = require('./render-data.json')
 var extend = require('xtend')
 
 module.exports = function VModel() {
-	var noddityRoot = 'https://raw.githubusercontent.com/ArtskydJ/josephdykstra.com-content/master/'
-	var butler = new Butler(noddityRoot, new Level(), {
-		refreshEvery: 600000,
-		cacheCheckIntervalMs: 10000
-	})
-	var postTemplate = null
-	butler.getPost('post-template.html', function (err, post) {
+	var butler = new Butler('https://raw.githubusercontent.com/ArtskydJ/josephdykstra.com-content/master/', new Level())
+	var renderOpts = {
+		butler: butler,
+		linkifier: new Linkifier('/'),
+		data: renderData
+	}
+
+	butler.getPosts(function (err, posts) {
 		if (err) throw err
-		postTemplate = post
+		console.log('loaded ' + posts.length + ' posts')
 	})
+
 	return function (filename, cb) {
-		var renderOpts = {
-			butler: butler,
-			linkifier: Linkifier('/'),
-			data: renderData // not both places
-		}
-		if (filename.indexOf('.') == -1) filename += '.md'
+		butler.getPost('post-template.html', function (err, post) {
+			if (err) {
+				cb(err)
+			} else {
+				post.content = post.content.replace('{{{html}}}', '::' + filename + '::')
 
-		var post = extend(postTemplate)
-		post.metadata = extend(renderData, post.metadata)
-		post.content = post.content.replace('{{{html}}}', '::' + filename + '::')
-
-		render(post, renderOpts, cb)
+				render(post, renderOpts, cb)
+			}
+		})
 	}
 }
