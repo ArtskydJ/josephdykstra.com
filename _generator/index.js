84913fc80a4888346acb67ca8dc8c267c4430f48
Joseph Dykstra <josephdykstra@gmail.com>
2016-08-15 13:01:13 -0500

    Refactor the generator
    I think the intent is more obvious now.

--- _generator/index.js
+++ _generator/index.js
@@ -4,54 +4,50 @@ var map = require('map')
 var feed = require('./feeds.js')()
 var noddity = require('./noddity.js')()
 
-function resolvePath(filename) {
-	return path.resolve(__dirname, '..', filename)
-}
+noddity.getPosts(function (err, posts) {
+	if (err) throw err
+
+	// These don't need to be generated in the correct order
+	posts.forEach(save)
+
+	// The posts start in the correct order, but `feed` does not sort the posts by date
+	// (https://github.com/jpmonette/feed/issues/28) so they must be added in the correct
+	// order. That's why I'm using `map`; to render and add them in series.
+
+	// Take last 5, and put the most recent first
+	var feedPosts = posts.slice(-5).reverse()
+
+	map(noddity.renderFeed, feedPosts, function (err, htmlPostFeeds) {
+		if (err) throw err
+
+		feedPosts.forEach(function (post, i) {
+			feed.add(post, htmlPostFeeds[i])
+		})
+
+		writeFileSync('feed.atom', feed.renderAtom())
+		writeFileSync('feed.rss', feed.renderRss())
+
+		noddity.getPost('index.md', function (err, post) {
+			if (err) throw err
+
+			save(post)
+		})
+	})
+})
 
 function save(post) {
 	noddity.renderHtml(post, function (err, html) {
 		if (err) throw err
 
 		var htmlFilename = post.filename.replace(/\.md$/, '.html')
-		fs.writeFileSync(resolvePath(htmlFilename), html)
+		writeFileSync(htmlFilename, html)
 	})
 }
 
-function getAndRenderAndSavePosts() {
-	noddity.getPosts(function (err, posts) {
-		if (err) throw err
-
-		// These don't need to be generated in the
-		// correct order
-		posts.forEach(save)
-
-		// The posts start in the correct order, but
-		// `feed` does not sort the posts by date
-		// (https://github.com/jpmonette/feed/issues/28)
-		// so they must be added in the correct order.
-		// That's why I'm using `map`; to render and
-		// add them in series.
-
-		// Take last 5, and put the most recent first
-		var feedPosts = posts.slice(-5).reverse()
-
-		map(noddity.renderFeed, feedPosts, function (err, htmlPostFeeds) {
-			if (err) throw err
-
-			feedPosts.forEach(function (post, i) {
-				feed.add(post, htmlPostFeeds[i])
-			})
-
-			fs.writeFileSync(resolvePath('feed.atom'), feed.renderAtom())
-			fs.writeFileSync(resolvePath('feed.rss'), feed.renderRss())
-
-			noddity.getPost('index.md', function (err, post) {
-				if (err) throw err
-
-				save(post)
-			})
-		})
-	})
+function writeFileSync(filename, data) {
+	fs.writeFileSync(resolvePath(filename), data)
 }
 
-getAndRenderAndSavePosts()
+function resolvePath(filename) {
+	return path.resolve(__dirname, '..', filename)
+}
