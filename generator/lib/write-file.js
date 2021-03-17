c60aa0097c855ecb7ac3b2ec5f132af319252723
ArtskydJ <josephdykstra@gmail.com>
2021-03-17 10:03:42 -0500

    More logging build info

--- generator/lib/write-file.js
+++ generator/lib/write-file.js
@@ -4,4 +4,5 @@ var path = require('path')
 module.exports = function writeFile(dir, filename, content) {
 	var outputPath = path.join(dir, filename)
 	fs.writeFileSync(outputPath, content)
+	console.log('Writing: ' + filename)
 }
