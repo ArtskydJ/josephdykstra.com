68e171e4a4a84fd4c17372148d49888e5b7ac446
Joseph Dykstra <1833684+ArtskydJ@users.noreply.github.com>
2024-03-13 23:13:17 -0500

    Update make-html-and-feeds.js

--- generator/make-html-and-feeds.js
+++ generator/make-html-and-feeds.js
@@ -22,11 +22,11 @@ noddity.getPost('resume.md', function (err, post) {
 		html = '<link href="./styles.css?" rel="stylesheet">' + html
 		writeFile(contentDir, 'resume-pdf.html', html)
 
-		require('fs').unlinkSync(contentDir + 'resume-pdf.html')
 		var cmd = 'wkhtmltopdf ' + contentDir + 'resume-pdf.html ' + contentDir + 'resume.pdf'
 		console.log('Running: ' + cmd)
 		cp.exec(cmd, function (err, stdout) {
 			if (err) throw err
+			require('fs').unlinkSync(contentDir + 'resume-pdf.html')
 			console.log('Finished running: ' + cmd)
 			cbWhenResumePdfIsGenerated()
 		})
