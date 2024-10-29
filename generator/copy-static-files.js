c1c1fdd3ea8c7ead66323502e29286ef412eefd9
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2024-10-29 11:28:02 -0500

    Stop copying json files to content dir

--- generator/copy-static-files.js
+++ generator/copy-static-files.js
@@ -4,8 +4,12 @@ var config = require('./config.json')
 
 var src = path.join(__dirname, config.relativeGeneratorToStaticPath)
 var dest = path.join(__dirname, config.relativeGeneratorToHtmlPath)
-var everythingExceptMarkdown = {
-	filter: [ '**', '!**/*.md', '!resume-pdf.html']
-}
 
-module.exports = copy(src, dest, everythingExceptMarkdown) // returns a promise
+module.exports = copy(src, dest, {
+	filter: [
+		'**',
+		'!**/*.md',
+		'!resume-pdf.html',
+		'!*.json',
+	]
+}) // returns a promise
